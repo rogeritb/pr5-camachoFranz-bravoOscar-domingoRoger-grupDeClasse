b877e44184350a73113e0146fc6ce19ea787f8ce
roger.domingo.7e7 <roger.domingo.7e7@itb.cat>
2024-01-22 13:00:26 +0100

    Hola

--- e2.py
+++ e2.py
@@ -1,24 +1,31 @@
 """
-Descripcion:
+e2:
 Programa que generi una llista de 100 nombres aleatoris entre 1 i 50.
 Obtenir la mitja dels nombres que es troben a les posicions parelles i la mitja del nombre de les posicions senars.
 """
 
 import random
 
-nombres = [random.randint(1, 9) for _ in range(100)]
+nombres = [random.randint(1, 50) for _ in range(100)]
+#nombres=[10,2,20,3,30]
 
 mitjana_parelles = 0
 mitjana_senars = 0
 
+parells = 0
+senars= 0
+
 for i in range(100):
     if i % 2 == 0:  # Posicions parelles
         mitjana_parelles += nombres[i]
+        parells += 1
     else:  # Posicions senars
         mitjana_senars += nombres[i]
+        senars += 1
 
-mitjana_parelles /= 50
-mitjana_senars /= 50
+
+mitjana_parelles /= parells
+mitjana_senars /= senars
 
 print("Llista de nombres aleatoris:", nombres)
 print("Mitjana de les posicions parelles:", mitjana_parelles)
--- e3.py
+++ e3.py
@@ -1,41 +1,6 @@
-def tradueix_catala(insult):
-    # Funció que tradueix l'insult en català a castellà
-    traductors = {
-        "Mocós": "Mocoso",
-        "Capsigrany": "Cabezón",
-        # Afegir aquí la resta d'insults en català
-    }
-    return traductors.get(insult, "No trobat")
-
-def tradueix_angles(insult):
-    # Funció que tradueix l'insult en català a anglès
-    # Implementar aquí la traducció utilitzant l'API de traducció DeepL
-    pass
-
-def tradueix_klingon(insult):
-    # Funció que tradueix l'insult en català a klingon
-    # Implementar aquí la traducció utilitzant l'API de traducció Tradukka
-    pass
-
-insults = {
-    "CAT": ["Mocós", "Capsigrany"],
-    # Afegir aquí la resta d'insults en català
-}
-
-traduccions = {
-    "ESP": [],
-    "ENG": [],
-    "KLI": [],
-}
-
-# Obtenir l'insult en català de l'usuari
-insult_catala = input("Escriu un insult en català: ")
-
-# Traducir l'insult a castellà, anglès i klingon
-for lang, trad in zip(["ESP", "ENG", "KLI"], [tradueix_catala, tradueix_angles, tradueix_klingon]):
-    traduccion = trad(insult_catala)
-    traduccions[lang].append(traduccion)
-
-# Mostrar les traduccions a l'usuari
-for lang, traduccions_lang in traduccions.items():
-    print(f"Traduccions a {lang}: {traduccions_lang}")
+"""
+Franz Camacho, Oscar Bravo, Roger Domingo
+e3:
+Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults en català i afegir la traducció en castellà, anglès i klingon
+El programa, demanarà a l’usuari que escrigui per teclat un insult, en català, i el mostrarà traduït a castellà, anglès i klingon.
+"""
